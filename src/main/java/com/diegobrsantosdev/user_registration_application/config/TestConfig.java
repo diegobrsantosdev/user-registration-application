417bfdb639a5f36cfe1b6b573d56a96a17cb1e88
diegobrsantosdev <diegobrsantosdev@gmail.com>
2025-09-16 14:34:50 -0300

    test-properties and dev-properties

--- src/main/java/com/diegobrsantosdev/user_registration_application/config/TestConfig.java
+++ src/main/java/com/diegobrsantosdev/user_registration_application/config/TestConfig.java
@@ -5,8 +5,10 @@ import com.diegobrsantosdev.user_registration_application.infrastructure.reposit
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("Test")
 public class TestConfig {
 
     @Bean
